bb48c37d42bbcbd1681d8f811b0826de571ef2b6
Nivogo <109019017+nivogo@users.noreply.github.com>
2025-01-23 02:10:54 +0300

    Update malKabul.js

--- pages/malKabul.js
+++ pages/malKabul.js
@@ -1,1 +1,13 @@
+// pages/malKabul.js
+import React from 'react';
 
+const MalKabul = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Mal Kabul Sayfası</h1>
+      <p>Bu sayfa henüz geliştirilmemiştir.</p>
+    </div>
+  );
+};
+
+export default MalKabul;

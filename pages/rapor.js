03781cc2d11d47c61c9e739010ed26e0915a29b2
Nivogo <109019017+nivogo@users.noreply.github.com>
2025-01-23 10:18:03 +0300

    Update rapor.js

--- pages/rapor.js
+++ pages/rapor.js
@@ -1,9 +1,26 @@
 // pages/rapor.js
-import React from 'react';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useAuth } from "../context/AuthContext"; // Güncellenmiş import
+import BackButton from "../components/BackButton";
 
 const Rapor = () => {
+  const router = useRouter();
+  const { user, userData } = useAuth();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/");
+    }
+  }, [user, router]);
+
+  if (!user || !userData) {
+    return <p>Yükleniyor...</p>;
+  }
+
   return (
     <div style={{ padding: '2rem' }}>
+      <BackButton />
       <h1>Rapor Sayfası</h1>
       <p>Bu sayfa henüz geliştirilmemiştir.</p>
     </div>

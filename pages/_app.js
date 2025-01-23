f7c5a129b6b5f0c550419d46d4ea689b51dc6cfb
Nivogo <109019017+nivogo@users.noreply.github.com>
2025-01-23 10:15:56 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,11 @@
 // pages/_app.js
-import { useEffect, useState, createContext, useContext } from "react";
-import { auth } from "../firebase/firebaseConfig";
-import { getUserData } from "../lib/firestore";
-import { onAuthStateChanged } from "firebase/auth";
-
-// Auth Context Oluşturma
-const AuthContext = createContext(null);
-
-// Auth Context'i Kullanmak İçin Hook
-export const useAuth = () => useContext(AuthContext);
+import { AuthProvider } from "../context/AuthContext";
 
 function MyApp({ Component, pageProps }) {
-  const [user, setUser] = useState(null);
-  const [userData, setUserData] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
-      if (firebaseUser) {
-        setUser(firebaseUser);
-        const data = await getUserData(firebaseUser.uid);
-        setUserData(data);
-        setLoading(false);
-      } else {
-        setUser(null);
-        setUserData(null);
-        setLoading(false);
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  if (loading) {
-    return <p>Yükleniyor...</p>;
-  }
-
   return (
-    <AuthContext.Provider value={{ user, userData }}>
+    <AuthProvider>
       <Component {...pageProps} />
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 

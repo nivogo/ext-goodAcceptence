0e37002c8accc94457237e0a35c77c4b7cd17738
Nivogo <109019017+nivogo@users.noreply.github.com>
2025-01-23 10:10:40 +0300

    Update malKabul.js

--- pages/malKabul.js
+++ pages/malKabul.js
@@ -1,9 +1,26 @@
 // pages/malKabul.js
-import React from 'react';
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useAuth } from "../_app"; // Auth Hook'u kullanıyoruz
+import BackButton from "../components/BackButton";
 
 const MalKabul = () => {
+  const router = useRouter();
+  const { user, userData } = useAuth();
+
+  useEffect(() => {
+    if (!user && !loading) {
+      router.push("/");
+    }
+  }, [user, router]);
+
+  if (!user || !userData) {
+    return <p>Yükleniyor...</p>;
+  }
+
   return (
     <div style={{ padding: '2rem' }}>
+      <BackButton />
       <h1>Mal Kabul Sayfası</h1>
       <p>Bu sayfa henüz geliştirilmemiştir.</p>
     </div>

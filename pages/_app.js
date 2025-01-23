708823b5712231f5c95921800e1fa526310fe150
Nivogo <109019017+nivogo@users.noreply.github.com>
2025-01-23 10:58:50 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // pages/_app.js
 import { AuthProvider } from "../context/AuthContext";
+import "../styles/globals.css"; // Eğer global stilleriniz varsa
 
 function MyApp({ Component, pageProps }) {
   return (

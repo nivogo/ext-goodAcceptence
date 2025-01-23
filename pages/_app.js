b9f48b33d0a5843c8a458f8472e33e4d201fbb3f
Nivogo <109019017+nivogo@users.noreply.github.com>
2025-01-23 12:41:35 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import { AuthProvider } from "../context/AuthContext";
 import "../styles/globals.css"; // Eğer global stilleriniz varsa
+import "../styles/Rapor.module.css"; // Rapor sayfası stilleri
 
 function MyApp({ Component, pageProps }) {
   return (

1713968bf7c3ffe4db9732513509443d76c8ee3c
Nivogo <109019017+nivogo@users.noreply.github.com>
2025-01-24 09:21:08 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import { AuthProvider } from "../context/AuthContext";
 import "../styles/globals.css"; // Eğer global stilleriniz varsa
 import "../styles/Rapor.module.css"; // Rapor sayfası stilleri
+import "../styles/BasariliKoliler.module.css"; // Başarılı Koliler sayfası stilleri
 
 function MyApp({ Component, pageProps }) {
   return (

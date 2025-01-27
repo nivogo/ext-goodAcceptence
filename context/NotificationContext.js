a3f89a05bed4a66bde79c3472d34a1f61efc7c8b
Nivogo <109019017+nivogo@users.noreply.github.com>
2025-01-27 13:07:17 +0300

    Create NotificationContext.js

---
+++ context/NotificationContext.js
@@ -0,0 +1,31 @@
+// context/NotificationContext.js
+import { createContext, useState, useContext } from "react";
+
+const NotificationContext = createContext();
+
+export const NotificationProvider = ({ children }) => {
+  const [notification, setNotification] = useState({
+    message: "",
+    type: "", // "success" | "error" vb.
+  });
+
+  /**
+   * Bildirimi belirli bir süre için gösterir (örn. 3 saniye).
+   * type: "success" veya "error" gibi mesajın türünü belirlemek için kullanılır.
+   */
+  const showNotification = (message, type = "success") => {
+    setNotification({ message, type });
+    // 3 saniye sonra bildirimi kapatalım
+    setTimeout(() => {
+      setNotification({ message: "", type: "" });
+    }, 3000);
+  };
+
+  return (
+    <NotificationContext.Provider value={{ notification, showNotification }}>
+      {children}
+    </NotificationContext.Provider>
+  );
+};
+
+export const useNotification = () => useContext(NotificationContext);

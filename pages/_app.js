21a22bd90b35bd2e53e97e9535a212dc172c4737
Nivogo <109019017+nivogo@users.noreply.github.com>
2025-01-27 13:09:25 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 // pages/_app.js
 import { AuthProvider } from "../context/AuthContext";
+import { NotificationProvider } from "../context/NotificationContext";
+import Notification from "../components/Notification";
 import "../styles/globals.css"; // Eğer global stilleriniz varsa
 import "../styles/Rapor.module.css"; // Rapor sayfası stilleri
 import "../styles/BasariliKoliler.module.css"; // Başarılı Koliler sayfası stilleri
@@ -7,7 +9,11 @@ import "../styles/BasariliKoliler.module.css"; // Başarılı Koliler sayfası s
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <NotificationProvider>
+        <Component {...pageProps} />
+        {/* Her sayfada bildirimleri gösterecek bileşen */}
+        <Notification />
+      </NotificationProvider>
     </AuthProvider>
   );
 }
